c0fc7b1c8049b3087e3fbd32ba495721c13c5d7a
Hazael Sanchez <hazaelsan@gmail.com>
2021-05-21 20:42:15 -0700

    Improve `corprelay/connect:connect_test` coverage

--- relay/request/corprelay/connect/connect_test.go
+++ relay/request/corprelay/connect/connect_test.go
@@ -12,62 +12,66 @@ var sid = uuid.MustParse("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee")
 
 func TestNew(t *testing.T) {
 	testdata := []struct {
+		name string
 		uri  string
 		want *Request
 		ok   bool
 	}{
 		{
-			uri: "/connect?ack=0&pos=0&try=1&sid=" + sid.String(),
+			name: "good",
+			uri:  "/connect?ack=0&pos=0&try=1&sid=" + sid.String(),
 			want: &Request{
 				SID: sid,
 				try: 1,
 			},
 			ok: true,
 		},
-		// Non-zero ack.
 		{
-			uri: "/connect?ack=1&pos=0&try=1&sid=" + sid.String(),
+			name: "non-zero ack",
+			uri:  "/connect?ack=1&pos=0&try=1&sid=" + sid.String(),
 		},
-		// Non-zero pos.
 		{
-			uri: "/connect?ack=0&pos=1&try=1&sid=" + sid.String(),
+			name: "non-zero pos",
+			uri:  "/connect?ack=0&pos=1&try=1&sid=" + sid.String(),
 		},
-		// Bad ack.
 		{
-			uri: "/connect?ack=foo&pos=0&try=1&sid=" + sid.String(),
+			name: "bad ack",
+			uri:  "/connect?ack=foo&pos=0&try=1&sid=" + sid.String(),
 		},
-		// Bad pos.
 		{
-			uri: "/connect?ack=0&pos=foo&try=1&sid=" + sid.String(),
+			name: "bad pos",
+			uri:  "/connect?ack=0&pos=foo&try=1&sid=" + sid.String(),
 		},
-		// Bad try.
 		{
-			uri: "/connect?ack=0&pos=0&try=foo&sid=" + sid.String(),
+			name: "bad try",
+			uri:  "/connect?ack=0&pos=0&try=foo&sid=" + sid.String(),
 		},
-		// Bad sid.
 		{
-			uri: "/connect?ack=0&pos=0&try=1&sid=foobar",
+			name: "bad sid",
+			uri:  "/connect?ack=0&pos=0&try=1&sid=foobar",
 		},
 	}
 	for _, tt := range testdata {
 		req, err := http.NewRequest("GET", tt.uri, nil)
 		if err != nil {
-			t.Errorf("http.NewRequest(%v) error = %v", tt.uri, err)
+			t.Errorf("http.NewRequest(%v) error = %v", tt.name, err)
 			continue
 		}
 		got, err := New(req)
 		if err != nil {
 			if tt.ok {
-				t.Errorf("New(%v) error = %v", tt.uri, err)
+				t.Errorf("New(%v) error = %v", tt.name, err)
 			}
 			continue
 		}
 		if !tt.ok {
-			t.Errorf("New(%v) error = nil", tt.uri)
+			t.Errorf("New(%v) error = nil", tt.name)
 		}
-
+		if got.String() != sid.String() {
+			t.Errorf("New(%v) SID = %v, want %v", tt.name, got, sid)
+		}
 		if diff := pretty.Compare(got, tt.want); diff != "" {
-			t.Errorf("New(%v) diff (-got +want):\n%v", tt.uri, diff)
+			t.Errorf("New(%v) diff (-got +want):\n%v", tt.name, diff)
 		}
 	}
 }
